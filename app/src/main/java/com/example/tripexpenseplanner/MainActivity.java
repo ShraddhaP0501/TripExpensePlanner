d8a7b935a58b98b994e56f623722d9808d749aba
Shraddha Prajapati <shraddha@gmail.com>
2026-09-14 16:06:26 +0530

    TripExpensePlanner project with basic Android structure and configuration

---
+++ app/src/main/java/com/example/tripexpenseplanner/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.tripexpenseplanner;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Entry point of the Trip Expense and Planner app.
+ * Step 1: Displays a simple welcome screen only.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
